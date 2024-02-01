eb4b493d2863236b7894b552b950528a3a07d5da
Tuyết Linh <73842823+tuyetlnhh@users.noreply.github.com>
2024-02-01 23:39:14 +0700

    add, delete, filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,163 +1,96 @@
-// services
-class HttpRequest {
-  constructor(apiUrl) {
-    this.apiUrl = apiUrl
-  }
-
-  async get(url) {
-    const response = await fetch(`${this.apiUrl}/${url}`)
-    return response.json();
-  }
-
-  async post(url, options = {}) {
-    // code something logic
-    const response = await fetch(`${this.apiUrl}/${url}`, {
-      method: 'POST',
-      ...options
-    })
-    return response.json();
-  }
-}
-const httpRequest = new HttpRequest('https://jsonplaceholder.typicode.com');
-
-
 const formSubmit = document.getElementById("issue-form");
-const issues = JSON.parse(localStorage.getItem('issues')) || [];
 const issuesList = document.getElementById('issues-list');
+let issues = JSON.parse(localStorage.getItem('issues')) || [];
 formSubmit.addEventListener("submit", handleAddTodo);
-
-// https://jsonplaceholder.typicode.com/todos -> get all list
-// https://jsonplaceholder.typicode.com/todos?_limit=10&_page=2 -> pagination
-// initialize
-async function initialize() {
-  const data = await httpRequest.get('todos?_limit=10&_page=1');
-  fetchIssues(data); 
-  // fetch('https://jsonplaceholder.typicode.com/todos?_limit=10&_page=1') // promise
-  //   .then(response => response.json()) // promise
-  //   .then(data => {
-  //     fetchIssues(data); 
-  //   })
-}
-
-initialize();
-
 // luu data
-function handleAddTodo(e) {
-  e.preventDefault();
+function handleAddTodo(event) {
+  event.preventDefault();
   const description = document.getElementById("issue-description").value;
   const severity = document.getElementById("issue-severity").value;
   const id = Date.now();
   const status = 'Open';
-    
+  formSubmit.reset();
   if (description.length === 0) {
     alert("Không được để trống");
     return;
   }
 
-  const issueItem = { id, title: description, severity, status };
-  // add todo -> call api (issueItem) to BE
-
-  // let issues = [];
-  // if (localStorage.getItem('issues')) {
-  //   issues = JSON.parse(localStorage.getItem('issues'));
-  // }
-  // issues.push(issue);
-  // localStorage.setItem('issues', JSON.stringify(issues));
+  // Store data in localStorage
+  const issueItem = { id, description, severity, status };
   const data = [...issues, issueItem];
+  console.log(data);
   localStorage.setItem('issues', JSON.stringify(data));
   fetchIssues(data);
+
+  // Issue list had changed so update it
+  issues = JSON.parse(localStorage.getItem('issues')) || [];
 }
 
-// handle delete
+// Delete data in localStorage
 function deleteIssue(todoId) {
-  console.log("deleteIssue -> todoId", todoId)
+  // Filter out the Issue ID
+  const remainingIssues = issues.filter(issue => ((issue.id) != todoId))
+  window.localStorage.removeItem('issues');
+  localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues(remainingIssues);
+  issues = JSON.parse(localStorage.getItem('issues')) || [];
+}
+
+//Close issue
+function closeIssue(todoId) {
+  // Find the issue by id and change status to close
+  const issue = issues.find(issue => ((issue.id) === todoId))
+  issue.status = 'Closed';
+  console.log('issues: ', issues);
+  // Change issue list and store it in localStorage
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues(issues);
+  issues = JSON.parse(localStorage.getItem('issues')) || [];
+}
+
+function filterByStatus(status) {
+  if (status === 'All') {
+    fetchIssues(issues);
+    return;
+  }
+  const filteredIssues = issues.filter(issue => issue.status == status);
+  console.log('filteredIssues: ', filteredIssues);
+  fetchIssues(filteredIssues);
 }
 
 // fetch data pure function
 function fetchIssues(data) {
-  if(data.length === 0) return;
-
+  if(data.length === 0) {
+    issuesList.innerHTML = '';
+    return;
+  }
   console.log('data: ', data)
-
+  // Init issuesList
   issuesList.innerHTML = '';
 
   for (var i = 0; i < data.length; i++) {
-    const { id, title, completed } = data[i];
-    issuesList.innerHTML += ` <section class="mt-3 mb-3">
-  <div class="issues-detail border border-dark-subtle">
-    <div class="id-status d-flex ps-2 p-1 bg-secondary-subtle border-bottom border-dark-subtle ">
-      <p class="id p-1">${id}</p>
-      <p class="fw-bold rounded-3 bg-secondary ms-2 text-white p-1">${completed}</p>
-    </div>
-
-    <div class="description ps-2 p-1">
-      <h3>${title}</h3>
-    </div>
-
-    <div class="status-group d-flex flex-row justify-content-end align-items-center">
-      <div class="status m-4">
-        <button class="btn btn-primary btn-dark" onclick="closeIssue(${id})" id="Close">Close</button>
-        <button class="btn btn-primary btn-danger" onclick="deleteIssue(${id})" id="Delete">Delete</button>
+    const { id, description, severity, status } = data[i];
+    issuesList.innerHTML += `
+    <div class="issues-detail border border-dark-subtle my-3">
+      <div class="id-status  ps-2 p-1 bg-secondary-subtle border-bottom border-dark-subtle ">
+        <p class="id p-1">ID: ${id}</p>
       </div>
-    </div>
-</section> `;
+
+      <div class="description ps-2 p-1">
+        <div class = d-flex>
+          <div class="badge text-wrap bg-primary">${severity}</div>
+          <div class="badge text-wrap ms-2 ${status === 'Closed' ? 'bg-secondary' : 'bg-info'}">${status}</div>
+        </div>
+        <h3 class = "${status === 'Closed' ? 'text-decoration-line-through font-italic' : ''}">${description}</h3>
+      </div>
+
+      <div class="status-group d-flex flex-row justify-content-end align-items-center">
+        <div class="status m-4">
+          <button class="btn btn-primary btn-dark" onclick="closeIssue(${id})" id="Close">Close</button>
+          <button class="btn btn-primary btn-danger" onclick="deleteIssue(${id})" id="Delete">Delete</button>
+        </div>
+      </div>
+    </div> `;
   }
 }
-
-
-  
-
-// example render todo list with pure js
-// const mockTodos = [
-//   {
-//     id: '2321312312122131',
-//     description: 'xx',
-//     severity: 'xx',
-//     status: 'new'
-//   }
-// ]
-
-
-// function handleAddTodo() {
-//   const todoItem = {
-//     id: Date.now(),
-//     description: 'xxxxxxx',
-//     severity: 'low',
-//     status: 'new'
-//   }
-//   mockTodos.unshift(todoItem)
-// }
-
-
-// mapIssue.forEach((issue) => {
-//   issuesList.innerHTML += `
-//       <li id="issue-list-item--${issue.id}" class="issue-list-item">
-//           <div class="list-item-header">
-//               <div for="" class="list-item-title">${issue.id}</div>
-//               <div id="issueStatus" class="list-item-status">
-//                   ${issue.status}
-//               </div>
-//           </div>
-//           <div class="list-item-content">
-//               <h3 class="issue-name">${issue.description}</h3>
-//               <div class="list-item-severity">${issue.severity}</div>
-//               <div class="list-item-group-btn">
-//                   <button 
-//                       id="changeSttBtn" 
-//                       class="btn btn--close"
-//                       onclick="updateIssueStt(
-//                         '${issue.id}', '${issue.status}')"
-//                   >
-//                       ${issue.status === 'new' ? 'Close' : 'Open'}
-//                   </button>
-                  
-//                   <button 
-//                       class="btn btn--delete" 
-//                       onclick="deleteIssue('${issue.id}')"
-//                   >Delete</button>
-//               </div>
-//           </div>
-//       </li>
-//       <br>
-//   `;
-// });
+fetchIssues(issues); 
